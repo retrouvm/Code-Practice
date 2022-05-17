9ed54a8fdb3ae28214b046420c2fc958a3d3fcca
retrouvm <retrouvaillesm@gmail.com>
2022-05-17 10:38:16 -0500

    mod

---
+++ pckex.ipynb/__init__.py
@@ -0,0 +1,6 @@
+class Solution:
+    def utility(self, a, b):
+        #code here
+        a,b=b,a
+        
+        return(a,b);
---
+++ pckex.ipynb/swapnum.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def utility(self, a, b):
+        #code here
+        a,b=b,a
+        
+        return(a,b);
+
+    
+
+#a = int(input())
+#b = int(input())
+#oj = Solution();
+#oj.utility(a, b)
